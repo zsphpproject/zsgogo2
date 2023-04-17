80b73cf9162bc96c005415949e9b93c8b2a8c86a
张帅 <5346812+zhangshuaiii@user.noreply.gitee.com>
2023-04-17 15:08:07 +0800

    clientRequest修改

--- src/utils/ClientRequest.php
+++ src/utils/ClientRequest.php
@@ -14,15 +14,17 @@ class ClientRequest {
 
     /**
      * 发送请求
-     * @param string $baseUrl  接口跟路径
-     * @param string $path     接口uri
+     * @param string $baseUrl 接口跟路径
+     * @param string $path 接口uri
      * @param string $method
-     * @param array $param    参数
+     * @param array $param 参数
+     * @param array $headers
+     * @param string $request_id
      * @return mixed
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    public function send(string $baseUrl,string $path = "",string $method = "post",array $param = [],string $request_id = ""): mixed {
+    public function send(string $baseUrl,string $path = "",string $method = "post",array $param = [],array $headers = [],string $request_id = ""): mixed {
         $client = new Client([
             "base_uri" => $baseUrl,
             "timeout" => 10.0
@@ -30,6 +32,7 @@ class ClientRequest {
 
         $option[RequestOptions::JSON] = [];
         if (!empty($param)) $option[RequestOptions::JSON] = $param;
+        if (!empty($headers)) $option[RequestOptions::HEADERS] = $headers;
 
         Log::get("guzzle_request")->info(json_encode($option,JSON_UNESCAPED_UNICODE),["request_id" => $request_id]);
 
@@ -39,9 +42,13 @@ class ClientRequest {
             Log::get("guzzle_response")->info(json_encode($res,JSON_UNESCAPED_UNICODE),["request_id" => $request_id]);
 
             if ($response->getStatusCode() == 200){
-                if ($res["code"] != 0) {
+                if (isset($res["code"]) && $res["code"] != 0) {
                     Log::get("guzzle_response")->info("{$path}请求错误:" . $res["msg"],["request_id" => $request_id]);
                     throw new AppException(ErrorNums::INVALID_AUTH,$res["msg"]);
+                }
+                if (isset($res["status"]) && $res["status"] != 0){
+                    Log::get("guzzle_response")->info("{$path}请求错误:" . $res["message"],["request_id" => $request_id]);
+                    throw new AppException(ErrorNums::INVALID_AUTH,$res["message"]);
                 }
                 return $res["data"];
             }else{
