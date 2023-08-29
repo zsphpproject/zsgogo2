b8edb2fca361766c177f25a4e6f188a390135be7
张帅 <5346812+zhangshuaiii@user.noreply.gitee.com>
2023-08-29 17:31:16 +0800

    SmsUtil.php更新

--- src/utils/SmsUtil.php
+++ src/utils/SmsUtil.php
@@ -4,6 +4,7 @@ namespace Zsgogo\utils;
 
 use App\common\constant\ErrorNums;
 use Overtrue\EasySms\EasySms;
+use Overtrue\EasySms\PhoneNumber;
 use Overtrue\EasySms\Exceptions\InvalidArgumentException;
 use Overtrue\EasySms\Exceptions\NoGatewayAvailableException;
 use Zsgogo\exception\AppException;
@@ -22,16 +23,33 @@ class SmsUtil {
      * @param string $code
      * @param string $template
      * @param string $content
-     * @return void
+     * @return array
      */
-    public function send(string $mobile,string $code, string $template,string $content = ""): void {
+    public function send(string $mobile,string $code, string $template,string $content = ""): array {
         $easySms = new EasySms($this->config);
         try {
-            $easySms->send($mobile, ['content' => $content ?? '您的验证码为: ' . $code, 'template' => $template, 'data' => ['code' => $code],]);
+            return $easySms->send($mobile, ['content' => $content ?? '您的验证码为: ' . $code, 'template' => $template, 'data' => ['code' => $code],]);
         } catch (InvalidArgumentException|NoGatewayAvailableException $e) {
             throw new AppException(ErrorNums::EXECUTION_ERROR,json_encode($e->getExceptions()));
         }
     }
 
-
+    /**
+     * 发送验证码-国际
+     * @param string $mobile
+     * @param string $areaCode
+     * @param string $code
+     * @param string $template
+     * @param string $content
+     * @return array
+     */
+    public function sendInternational(string $mobile,string $areaCode,string $code, string $template,string $content = "") :array {
+        $number = new PhoneNumber($mobile, $areaCode);
+        $easySms = new EasySms($this->config);
+        try {
+            return $easySms->send($number, ['content' => $content ?? '您的验证码为: ' . $code, 'template' => $template, 'data' => ['code' => $code],]);
+        } catch (InvalidArgumentException|NoGatewayAvailableException $e) {
+            throw new AppException(ErrorNums::EXECUTION_ERROR,json_encode($e->getExceptions()));
+        }
+    }
 }
